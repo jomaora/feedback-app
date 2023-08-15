628c4df26585c04a853a18fe4e1d7d2e55c3981b
Joan <jomaora@gmail.com>
2023-08-15 17:43:48 +0200

    session 25-26 / Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage';
 
 import FeedbackData from './data/FeedbackData'
 
@@ -10,7 +12,6 @@ function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
   const addFeedback = (newFeedback) => {
-    console.log('---->', newFeedback);
     setFeedback([newFeedback, ...feedback])
   }
 
@@ -21,14 +22,25 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route 
+            exact
+            path='/' 
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }
+          />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About this project</h1>
+      <p>
+        <a href="/">Back to home</a>
+      </p>
+    </Card>
+  )
+}
+
+export default AboutPage
